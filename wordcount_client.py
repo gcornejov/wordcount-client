9af5f43f9c89e504c79ba9c117d2e7e86a195036
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-21 01:05:22 -0300

    Add code to read json response and format print

--- wordcount_client.py
+++ wordcount_client.py
@@ -1,6 +1,8 @@
-import requests
+import requests, json
 
 files = {'file': open('./data-in/document.txt', 'rb')}
 r = requests.post('http://127.0.0.1:5000/wordcount', files=files)
 
-print(r.text)
+words = json.loads(r.text)
+for word, amount in words.items():
+	print("{} {}".format(word, amount))
