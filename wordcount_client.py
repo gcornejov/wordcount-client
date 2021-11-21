fca13a72685ececfcc57d69db22668c415af400e
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-21 01:39:00 -0300

    Add validations for files existence

--- wordcount_client.py
+++ wordcount_client.py
@@ -1,14 +1,22 @@
-import argparse, requests, yaml, json
+import sys, os, argparse, requests, yaml, json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', '-f', help="text file containing document", type=str)
 args = parser.parse_args()
+
+if not os.path.isfile("./config.yaml"):
+	print('Configuration file: \'./config.yaml\', not found.')
+	sys.exit()
 
 with open("config.yaml", "r") as ymlfile:
 		cfg = yaml.safe_load(ymlfile)
 
 file_name = '{0}/{1}'.format(cfg['location']['directory'], args.file)
 url = cfg['endpoint']['url']
+
+if not os.path.isfile(file_name):
+	print('File: \'{0}\', not found.'.format(file_name))
+	sys.exit()
 
 files = {'file': open(file_name, 'rb')}
 r = requests.post(url, files=files)
